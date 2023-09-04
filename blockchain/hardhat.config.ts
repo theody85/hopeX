1092319a1f39be2f080a82c4d75d61f92c15d02f
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-04 12:23:14 +0000

    Add reporter

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
+import "hardhat-gas-reporter";
 
 dotenv.config();
 
@@ -9,8 +10,16 @@ const config: HardhatUserConfig = {
   networks: {
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [`${process.env.PRIVATE_KEY}`]
+      accounts: [`${process.env.PRIVATE_KEY}`],
     },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD", // currency to show
+    outputFile: "gas-report.txt", // optional
+    noColors: true, //optional
+    coinmarketcap: process.env.ALCHEMY_API_KEY, //to fetch gas data
+    token: "MATIC", // for polygon blockchain(optional).
   },
 };
 
